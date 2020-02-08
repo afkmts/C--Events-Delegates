397579c5fa52a369541a7d26edd39e1475bed670
Asmerom <51678020+afkmts@users.noreply.github.com>
2020-02-08 17:15:18 +0300

    Update VideoEncoder.cs

--- VideoEncoder.cs
+++ VideoEncoder.cs
@@ -53,18 +53,4 @@ namespace EventsDelegates
         public Video video { get; set; }
     }
 
-
-    //The dervived classes of VideoEncoder can also use the event handler
-    //Because it is declared as virtual in Base class
-    class DerivedVideoEncoder : VideoEncoder
-    {
-        protected override void onVideoEncoded(Video video)
-        {
-            //Do something private to this class
-            Console.WriteLine("The derived class have done some thing...");
-
-            //And then Call base class method to fire the event
-            base.onVideoEncoded(video);
-        }
-    }
 }
