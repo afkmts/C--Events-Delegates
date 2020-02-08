7a12c6d96426e7ede9abf4a3dd2554084273d59f
Asmerom <51678020+afkmts@users.noreply.github.com>
2020-02-08 17:17:52 +0300

    Add files via upload

---
+++ DerivedVideoEncoder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EventsDelegates
+{
+    //The dervived classes of VideoEncoder can also use the event handler
+    //Because it is declared as virtual in Base class 
+    //this class can override the event handler method in the base class
+    class DerivedVideoEncoder : VideoEncoder
+    {
+        protected override void onVideoEncoded(Video video)
+        {
+            //Do something private to this class
+            Console.WriteLine("The derived class have done some thing...");
+
+            //And then Call base class method to fire the event
+            base.onVideoEncoded(video);
+        }
+    }
+}
---
+++ VideoEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EventsDelegates
+{
+    //We can also create our own VideoEventArgs
+    //This holds message about the event in this case the Title of the video encoded
+    //So that the notified methods can utilize this data for further processing
+    public class VideoEventArgs : EventArgs
+    {
+        public Video video { get; set; }
+    }
+}
